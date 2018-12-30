611ca9a46c4e2b9d03f048b4bff7677baea98ec0
Remus1992 <Remington.Henderson@gmail.com>
2018-12-30 07:33:46 -0800

    Sending Updates

--- src/containers/CheckOut/CheckOut.js
+++ src/containers/CheckOut/CheckOut.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
@@ -43,21 +43,25 @@ class Checkout extends Component {
     };
 
     render() {
-        return (
-            <div>
-                <CheckoutSummary
-                    ingredients={this.props.ings}
-                    checkoutCancelled={this.checkoutCancelledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler}/>
-                <Route
-                    path={this.props.match.path + "/contact-data"}
-                    // instead of passing ContactData as a 'component'
-                    // we are rendering it manually to pass props
-                    // component={ContactData}
-                    // passing along props to get history element
-                    component={ContactData}/>
-            </div>
-        )
+        let summary = <Redirect to='/'/>;
+        if (this.props.ings) {
+            summary = (
+                <div>
+                    <CheckoutSummary
+                        ingredients={this.props.ings}
+                        checkoutCancelled={this.checkoutCancelledHandler}
+                        checkoutContinued={this.checkoutContinuedHandler}/>
+                    <Route
+                        path={this.props.match.path + "/contact-data"}
+                        // instead of passing ContactData as a 'component'
+                        // we are rendering it manually to pass props
+                        // component={ContactData}
+                        // passing along props to get history element
+                        component={ContactData}/>
+                </div>
+            )
+        }
+        return summary;
     }
 }
 
