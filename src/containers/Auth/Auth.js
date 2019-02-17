628a0a2bd8a07260da586194bcf8631c88aa6a4b
Remus1992 <Remington.Henderson@gmail.com>
2019-02-17 12:55:28 -0800

    Sending Auth Updates

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux'
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
-
 import classes from './Auth.css'
+import * as actions from '../../store/actions/';
 
 class Auth extends Component {
     state = {
@@ -39,6 +40,58 @@ class Auth extends Component {
         }
     };
 
+    checkValidity(value, rules) {
+        let isValid = true;
+
+        // unnecessary step as we have added 'validation: {}' to 'select'
+        // but is instead an alternative
+        if (!rules) {
+            return true;
+        }
+
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        return isValid;
+    }
+
+    inputChangedHandler = (event, controlName) => {
+        const updatedControls = {
+            ...this.state.controls,
+            [controlName]: {
+                ...this.state.controls[controlName],
+                value: event.target.value,
+                valid: this.checkValidity(event.target.value, this.state.controls[controlName]),
+                touched: true
+            }
+        };
+        this.setState({controls: updatedControls})
+    };
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value)
+    }
+
     render() {
         const formElementsArray = [];
         for (let key in this.state.controls) {
@@ -62,7 +115,7 @@ class Auth extends Component {
         ));
         return (
             <div className={classes.Auth}>
-                <form>
+                <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType='Success'>Submit</Button>
                 </form>
@@ -71,4 +124,10 @@ class Auth extends Component {
     }
 }
 
-export default Auth
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password))
+    };
+};
+
+export default connect(null, mapDispatchToProps)(Auth);
