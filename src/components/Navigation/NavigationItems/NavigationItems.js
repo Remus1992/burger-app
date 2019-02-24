71319911d0a3afa9cb9d2b1484f73e17a694fcbc
Remus1992 <Remington.Henderson@gmail.com>
2019-02-23 16:57:14 -0800

    Sending Logout Info

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,7 +8,9 @@ const navigationItems = () => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link='/'>Burger Builder</NavigationItem>
         <NavigationItem link='/orders'>Orders</NavigationItem>
-        <NavigationItem link='/auth'>Authenticate</NavigationItem>
+        { !props.isAuthenticated
+            ? <NavigationItem link='/auth'>Authenticate</NavigationItem>
+            : <NavigationItem link='/logout'>Logout</NavigationItem>}
     </ul>
 );
 
