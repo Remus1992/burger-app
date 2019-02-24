4bf25fd1b4549096edaf8ffd6352fb88f405107c
Remus1992 <Remington.Henderson@gmail.com>
2019-02-23 22:21:41 -0800

    Sending Auth Redirects

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,9 +9,7 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../HOC/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
-// with an 'index' js file existing, we don't technically have to list it
-// since react will look for an 'index'
-import * as actions from '../../store/actions/index';
+import * as actions from '../../store/actions/';
 
 
 class BurgerBuilder extends Component {
@@ -22,18 +20,7 @@ class BurgerBuilder extends Component {
     };
 
     componentDidMount() {
-        // we have access to 'match' elements here
         console.log(this.props);
-        // don't forget to add '.json' to end of url from Firebase
-        // the following has been removed from here and send to an action
-        // so instead we call it below via props
-        // axios.get('https://react-my-burger-6f916.firebaseio.com/ingredients.json')
-        //     .then(response => {
-        //         this.setState({ingredients: response.data});
-        //     })
-        //     .catch(error => {
-        //         this.setState({error: true})
-        //     });
         this.props.onInitIngredients();
     }
 
@@ -45,48 +32,14 @@ class BurgerBuilder extends Component {
             .reduce((sum, el) => {
                 return sum + el;
             }, 0);
-        // this.setState({purchasable: sum > 0});
         return sum > 0;
     };
 
-    // addIngredientHandler = (type) => {
-    //     const oldCount = this.state.ingredients[type];
-    //     const updatedCount = oldCount + 1;
-    //     const updatedIngredients = {
-    //         ...this.state.ingredients
-    //     };
-    //     updatedIngredients[type] = updatedCount;
-    //     const priceAddition = INGREDIENT_PRICES[type];
-    //     const oldPrice = this.state.totalPrice;
-    //     const newPrice = oldPrice + priceAddition;
-    //     this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
-    //     this.updatePurchaseState(updatedIngredients);
-    // };
-
-    // removeIngredientHandler = (type) => {
-    //     const oldCount = this.state.ingredients[type];
-    //     if (oldCount <= 0) {
-    //         return;
-    //     }
-    //     const updatedCount = oldCount - 1;
-    //     const updatedIngredients = {
-    //         ...this.state.ingredients
-    //     };
-    //     updatedIngredients[type] = updatedCount;
-    //     const priceDeduction = INGREDIENT_PRICES[type];
-    //     const oldPrice = this.state.totalPrice;
-    //     const newPrice = oldPrice - priceDeduction;
-    //     this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
-    //     this.updatePurchaseState(updatedIngredients);
-    // };
-
-    // A standard 'this' doesn't work here because of how the function is executed
-    // One way to identify it is because it doesn't receive any inputs
-    // But note that render doesn't require any
     purchaseHandler = () => {
         if (this.props.isAuthenticated){
             this.setState({purchasing: true});
         } else {
+            this.props.onSetAuthRedirectPath('/checkout');
             this.props.history.push('/auth');
         }
     };
@@ -96,53 +49,12 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        // alert("You Continue!");
-
-        // this.setState({loading: true});
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'John Smith',
-        //         address: {
-        //             street: '1234 Road Ave',
-        //             zipCode: '56789',
-        //             country: 'USA'
-        //         },
-        //         email: 'test@test.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // };
-        // // '.json' is necessary for Firebase specifically
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({loading: false, purchasing: false});
-        //     })
-        //     .catch(error => {
-        //         this.setState({loading: false, purchasing: false});
-        //     });
-
-        // we commented out the above because we don't want to push to fireserve on this page
-        // anymore. We instead want to go to a checkout page and push it from there.
-        // We then copy/pasted it to ContactData.js and adjusted the code appropriately
-        // const queryParams = [];
-        // for (let i in this.state.ingredients) {
-        //     queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-        // }
-        // // this line below was added after we copy/pasted the above code to ContactData.js
-        // queryParams.push('price=' + this.state.totalPrice);
-        // const queryString = queryParams.join('&');
-        // this.props.history.push({
-        //     pathname: '/checkout',
-        //     search: '?' + queryString
-        // });
         this.props.onInitPurchase();
         this.props.history.push('/checkout');
     };
 
     render() {
         const disabledInfo = {
-            // ...this.state.ingredients
             ...this.props.ings
         };
         for (let key in disabledInfo) {
@@ -170,9 +82,6 @@ class BurgerBuilder extends Component {
                 ingredients={this.props.ings}
                 price={this.props.price}/>;
         }
-        // if (this.state.loading) {
-        //     orderSummary = <Spinner/>
-        // }
 
         return (
             <Aux>
@@ -199,7 +108,8 @@ const mapDispatchToProps = dispatch => {
         onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
         onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
         onInitIngredients: () => dispatch(actions.initIngredients()),
-        onInitPurchase: () => dispatch(actions.purchaseInit())
+        onInitPurchase: () => dispatch(actions.purchaseInit()),
+        onSetAuthRedirectPath: (path) => dispatch(actions.setAuthRedirectPath(path))
     }
 };
 
