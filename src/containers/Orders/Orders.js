3d324fdf80e84e0d5874aeca7271633b440ad0d1
Remus1992 <Remington.Henderson@gmail.com>
2019-02-26 19:04:03 -0800

    Sending Order Authentication

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -8,27 +8,8 @@ import * as actions from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
-    // state = {
-    //     orders: [],
-    //     loading: true
-    // };
-
     componentDidMount() {
-        // axios.get('/orders.json')
-        //     .then(res => {
-        //         const fetchedOrders = [];
-        //         for (let key in res.data) {
-        //             fetchedOrders.push({
-        //                 ...res.data[key],
-        //                 id: key
-        //             });
-        //         }
-        //         this.setState({loading: false, orders: fetchedOrders});
-        //     })
-        //     .catch(err => {
-        //         this.setState({loading: false});
-        //     });
-        this.props.onFetchOrder(this.props.token);
+        this.props.onFetchOrder(this.props.token, this.props.userId);
     }
 
     render() {
@@ -53,13 +34,14 @@ const mapStateToProps = state => {
     return {
         orders: state.order.orders,
         loading: state.order.loading,
-        token: state.auth.token
+        token: state.auth.token,
+        userId: state.auth.userId
     }
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onFetchOrder: (token) => dispatch(actions.fetchOrders(token))
+        onFetchOrder: (token, userId) => dispatch(actions.fetchOrders(token, userId))
     }
 };
 export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
