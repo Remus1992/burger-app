1ecba6e7ad34ea18f834ef9962adcbcd34179af9
Remus1992 <Remington.Henderson@gmail.com>
2019-03-08 20:42:56 -0800

    Sending updateObject revisions

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -7,6 +7,7 @@ import Button from '../../components/UI/Button/Button';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Auth.css'
 import * as actions from '../../store/actions/';
+import {updateObject} from '../../shared/utility';
 
 class Auth extends Component {
     state = {
@@ -84,15 +85,13 @@ class Auth extends Component {
     }
 
     inputChangedHandler = (event, controlName) => {
-        const updatedControls = {
-            ...this.state.controls,
-            [controlName]: {
-                ...this.state.controls[controlName],
+        const updatedControls = updateObject(this.state.controls, {
+            [controlName]: updateObject(this.state.controls[controlName], {
                 value: event.target.value,
                 valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
                 touched: true
-            }
-        };
+            })
+        });
         this.setState({controls: updatedControls})
     };
 
@@ -144,7 +143,7 @@ class Auth extends Component {
 
         let authRedirect = null;
         if (this.props.isAuthenticated) {
-            authRedirect = <Redirect to={this.props.authRedirectPath} />
+            authRedirect = <Redirect to={this.props.authRedirectPath}/>
         }
 
         return (
