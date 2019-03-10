b78e7ea4b6aab6570c75dfea589775f4af3544bd
Remus1992 <Remington.Henderson@gmail.com>
2019-03-10 16:55:20 -0700

    Removing Unnecessary Code

--- src/containers/CheckOut/CheckOut.js
+++ src/containers/CheckOut/CheckOut.js
@@ -4,41 +4,8 @@ import {connect} from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
-// import * as actions from '../../store/actions/index';
 
 class Checkout extends Component {
-    // state = {
-    //     ingredients: null,
-    //     price: 0
-    // };
-
-    // changed this from componentDidMount to allow for ingredients to be sent along with
-    // 'null' initialized state
-    // componentWillMount() {
-    //     const query = new URLSearchParams(this.props.location.search);
-    //     const ingredients = {};
-    //     let price = 0;
-    //     for (let param of query.entries()) {
-    //         // ['salad', '1']
-    //         // this loop adds ingredients to the empty dict above, so we need
-    //         // to build an if statement to account for the price that we passed along
-    //         // in the queryParams from BurgerBuilder.js
-    //         // the current method below though is a 'work around' and isn't the most efficient
-    //         if (param[0] === 'price') {
-    //             price = param[1]
-    //         } else {
-    //             ingredients[param[0]] = +param[1]
-    //         }
-    //     }
-    //     this.setState({ingredients: ingredients, totalPrice: price});
-    //     console.log(ingredients);
-    //     console.log(price)
-    // }
-
-    // componentWillMount() {
-    //     this.props.onInitPurchase();
-    // }
-
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
     };
@@ -60,10 +27,6 @@ class Checkout extends Component {
                         checkoutContinued={this.checkoutContinuedHandler}/>
                     <Route
                         path={this.props.match.path + "/contact-data"}
-                        // instead of passing ContactData as a 'component'
-                        // we are rendering it manually to pass props
-                        // component={ContactData}
-                        // passing along props to get history element
                         component={ContactData}/>
                 </div>
             )
@@ -78,15 +41,5 @@ const mapStateToProps = state => {
         purchased: state.order.purchased
     }
 };
-
-// we don't need mapDispatchToProps because we are not dispatching anything
-// we do navigate via these props but we use React-Router for this
-// we now have it because we added componentdidmount logic
-
-// const mapDispatchToProps = dispatch => {
-//     return {
-//         onInitPurchase: () => dispatch(actions.purchaseInit())
-//     };
-// };
 
 export default connect(mapStateToProps)(Checkout);
