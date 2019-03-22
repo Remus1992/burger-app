a9ab6555697c618538b75c5206bfb6cb1b24f37b
Remus1992 <Remington.Henderson@gmail.com>
2019-03-21 17:57:43 -0700

    Sending Testing

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,12 @@
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import NavigationItems from './NavigationItems'
+
+configure({adapter: new Adapter()});
+
+describe('<NavigationItems />', () => {
+    it('should render two <NavigationItem /> elements if not authenticated', () => {
+        const wrapper = shallow(<NavigationItems/>)
+    });
+});
